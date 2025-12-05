e5d26c005c74464df3c4e1ff384394c02a3b9ba4
thaodo <thaodo2512@gmail.com>
2025-12-05 12:23:22 +0700

    update docker

--- docker/jetbot/build.sh
+++ docker/jetbot/build.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
+set -e
 
-# Build the docker image that is contained learning_lacer from jetbot:juputer-[JETBOT_VERSION]-[L4T_VERSION]
-JUPYTER_TAG_NAME=$(sudo docker images jetbot/jetbot | grep -P jupyter)
-export JETBOT_VERSION=$(echo "$JUPYTER_TAG_NAME" | cut -f 8 -d ' ' | cut -f 2 -d '-')
+# --- CONFIGURATION ---
+# We pin these versions because official Jetbot images for 32.7.6 do not exist.
+# We will use the 32.4.4 container on your 32.7.6 system.
+export JETBOT_VERSION="0.4.2"
+export BASE_L4T_VERSION="32.4.4"
 
-L4T_VERSION_STRING=$(head -n 1 /etc/nv_tegra_release)
-L4T_RELEASE=$(echo "$L4T_VERSION_STRING" | cut -f 2 -d ' ' | grep -Po '(?<=R)[^;]+')
-L4T_REVISION=$(echo "$L4T_VERSION_STRING" | cut -f 2 -d ',' | grep -Po '(?<=REVISION: )[^;]+')
+# We can still label the output image with your actual system version if you want,
+# or just keep it consistent with the base. Let's use the base to avoid confusion.
+export OUTPUT_TAG="32.4.4"
 
-export JETBOT_DOCKER_REMOTE=jetbot
-export L4T_VERSION="$L4T_RELEASE.$L4T_REVISION"
+export JETBOT_DOCKER_REMOTE="jetbot"
 
-echo "Building docker image for jetbot:jupyter-$JETBOT_VERSION-$L4T_VERSION"
+echo "---------------------------------------"
+echo "Building learning_racer for Jetson Nano"
+echo "Base Image: $JETBOT_DOCKER_REMOTE/jetbot:jupyter-$JETBOT_VERSION-$BASE_L4T_VERSION"
+echo "Target Tag: learning_racer:$OUTPUT_TAG"
+echo "---------------------------------------"
 
+# --- BUILD ---
+# We force the BASE_IMAGE argument to match the image we pulled in Step 1
 sudo docker build \
-    --build-arg BASE_IMAGE=$JETBOT_DOCKER_REMOTE/jetbot:jupyter-"$JETBOT_VERSION"-"$L4T_VERSION" \
-    -t learning_racer:"$L4T_VERSION" \
+    --build-arg BASE_IMAGE=$JETBOT_DOCKER_REMOTE/jetbot:jupyter-"$JETBOT_VERSION"-"$BASE_L4T_VERSION" \
+    -t learning_racer:"$OUTPUT_TAG" \
     -f Dockerfile \
-    ../.. #learning_racer repo root as context(airc-rl-agent/docker/jetbot)
+    ../..
--- docker/jetbot/enable.sh
+++ docker/jetbot/enable.sh
@@ -3,16 +3,15 @@
 # Enable the Learning Racer container.
 # Usage: ./enable.sh /home/jetbot
 
-
 WORKSPACE=$1
 JETBOT_CAMERA=${2:-opencv_gst_camera}
 
-L4T_VERSION_STRING=$(head -n 1 /etc/nv_tegra_release)
-L4T_RELEASE=$(echo "$L4T_VERSION_STRING" | cut -f 2 -d ' ' | grep -Po '(?<=R)[^;]+')
-L4T_REVISION=$(echo "$L4T_VERSION_STRING" | cut -f 2 -d ',' | grep -Po '(?<=REVISION: )[^;]+')
-export L4T_VERSION="$L4T_RELEASE.$L4T_REVISION"
-# set default swap limit as unlimited
+# --- FIX START: Hardcode the version we built ---
+# We ignore the system version (32.7.6) and use the one we built (32.4.4)
+export L4T_VERSION="32.4.4"
+# --- FIX END ---
 
+# set default swap limit as unlimited
 if [ -z "$JETBOT_LEARNING_RACER_MEMORY_SWAP" ];
 then
         export JETBOT_LEARNING_RACER_MEMORY_SWAP=-1
@@ -22,6 +21,10 @@ if [ -z "$JETBOT_LEARNING_RACER_MEMORY" ];
 then
   export JETBOT_LEARNING_RACER_MEMORY=2500m
 fi
+
+echo "Starting learning_racer container..."
+echo "Image: learning_racer:$L4T_VERSION"
+echo "Workspace: $WORKSPACE"
 
 sudo docker run -it -d \
       --restart always \
